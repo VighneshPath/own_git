625e2c6e50a9db277a3549d949bf32808fc7fcf8
MaheshAravindV <55615133+MaheshAravindV@users.noreply.github.com>
2022-11-13 06:37:54 +0000

    refactor: make each command a member function

--- mgit.kt
+++ mgit.kt
@@ -4,44 +4,41 @@ import java.nio.file.StandardCopyOption
 import java.io.File
 import java.security.MessageDigest
 
-class MGit(commands: Array<String>) {
-    val cmd1 = commands[0]
-    init {
-        if(cmd1.equals("init")){
-            if(!Files.isDirectory(Paths.get(".mgit"))){
-                // Create info, objects, refs (With heads and tags) folders and HEAD and config files
-                Files.createDirectories(Paths.get(".mgit", "objects"))
-                Files.createDirectories(Paths.get(".mgit", "info"))
-                Files.createDirectories(Paths.get(".mgit", "refs", "heads"))
-                Files.createDirectories(Paths.get(".mgit", "refs", "tags"))
-                Files.createFile(Paths.get(".mgit", "HEAD"));
-                Files.createFile(Paths.get(".mgit", "config"))
-            }
-            println("Initialized mgit Successfully.")
+class MGit() {
+    fun CMD__init(){
+        if(!Files.isDirectory(Paths.get(".mgit"))){
+            // Create info, objects, refs (With heads and tags) folders and HEAD and config files
+            Files.createDirectories(Paths.get(".mgit", "objects"))
+            Files.createDirectories(Paths.get(".mgit", "info"))
+            Files.createDirectories(Paths.get(".mgit", "refs", "heads"))
+            Files.createDirectories(Paths.get(".mgit", "refs", "tags"))
+            Files.createFile(Paths.get(".mgit", "HEAD"));
+            Files.createFile(Paths.get(".mgit", "config"))
         }
-        else if(cmd1.equals("hash-object")){
-            try{
-                val fileContent = String(Files.readAllBytes(Paths.get(commands[1])))
-                val hash = MessageDigest.getInstance("SHA-1").digest(fileContent.toByteArray()).joinToString(""){"%02x".format(it)}
-                Files.copy(Paths.get(commands[1]), Paths.get(".mgit", "objects", hash) , StandardCopyOption.REPLACE_EXISTING)
-                println(hash)
-            }
-            catch(e : Exception){
-                println(e)
-            }
+        println("Initialized mgit Successfully.")
+    }
+
+    fun CMD__hash_object(path : String){
+        try{
+            val fileContent = String(Files.readAllBytes(Paths.get(path)))
+            val hash = MessageDigest.getInstance("SHA-1").digest(fileContent.toByteArray()).joinToString(""){"%02x".format(it)}
+            Files.copy(Paths.get(path), Paths.get(".mgit", "objects", hash) , StandardCopyOption.REPLACE_EXISTING)
+            println(hash)
         }
-        else if(cmd1.equals("cat-file")){
-            val objectName = commands[1]
-            try{
-                val content : String = File(Paths.get(".mgit","objects",objectName).toString()).readText()
-                println(content)
-            }
-            catch(e : Exception){
-                println(e)
-            }
+        catch(e : Exception){
+            println(e)
         }
     }
-    
+
+    fun CMD__cat_file(object_hash : String){
+        try{
+            val content : String = File(Paths.get(".mgit","objects",object_hash).toString()).readText()
+            println(content)
+        }
+        catch(e : Exception){
+            println(e)
+        }
+    }    
 }
 
 
@@ -50,6 +47,19 @@ fun main(args: Array<String>) {
         print("Please add some command line arguments")
         return
     }
-    var mgit_obj = MGit(args)      
+    val MG = MGit()
+    val command = args[0]
+
+    when(command){
+        "init" -> MG.CMD__init()
+        "hash-object" -> {
+            val path = args[1]
+            MG.CMD__hash_object(path)
+        }
+        "cat-file" -> {
+            val object_hash = args[1]
+            MG.CMD__cat_file(object_hash)
+        }
+    }
 }
 
